d96c28405440e0a9a8d47131b7196ede69362f00
ivankras <ivankras@hotmail.com>
2021-11-06 22:49:54 -0500

    Saturday evening

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
     before_action :require_logged_in
+    before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
     def index
         @tweets = Current.user.tweets
@@ -9,4 +10,49 @@ class TweetsController < ApplicationController
         @tweet = Tweet.new
     end
     
+    def create
+        @tweet = Current.user.tweets.create(tweet_params)
+        if @tweet.save
+          flash[:success] = "Tweet successfully scheduled"
+          redirect_to tweets_path
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @tweet.update(tweet_params)
+          flash[:success] = "Tweet was successfully updated"
+          redirect_to tweets_path
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+    
+    def destroy
+        if @tweet.destroy
+            flash[:success] = 'Tweet was unscheduled'
+            redirect_to tweets_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to tweets_path
+        end
+    end
+    
+
+    private
+    
+    def tweet_params
+        params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+    end
+
+    def set_tweet
+        @tweet = Current.user.tweets.find(params[:id])
+    end
 end
